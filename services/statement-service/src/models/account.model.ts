1ac10b3710d3d252979c02eba8aefc72d3e495c8
brendonmatos <brendonferreiradm@gmail.com>
2023-09-22 23:29:24 -0300

    feat: start statement service

---
+++ services/statement-service/src/models/account.model.ts
@@ -0,0 +1,11 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Movement } from './movement.model';
+
+@ObjectType()
+export class Account {
+  @Field(_ => Int)
+  id: number;
+
+  @Field(_ => [])
+  movements: Movement[];
+}
