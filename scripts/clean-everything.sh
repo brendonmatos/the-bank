c5b9300a01ebb073c4f3923b2890ed81fdcf7f52
brendonmatos <brendonferreiradm@gmail.com>
2023-10-03 22:12:38 -0300

    chore: improve clean scripts

--- scripts/clean-everything.sh
+++ scripts/clean-everything.sh
@@ -1,20 +1,28 @@
 docker kill $(docker ps -q)
 docker stop $(docker ps -a -q)
-
 docker rm -f $(docker ps -qa)
 docker rmi -f $(docker images -aq)
 docker image prune -f
 docker network prune --force
-
 docker volume prune -f
+docker system prune -f
+docker system prune --volumes -f
 
-rm -rf ./front/dist
 find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +
 find . -name '.angular' -type d -prune -exec rm -rf '{}' +
+find . -name '.cache' -type d -prune -exec rm -rf '{}' +
+find . -name '.gradle' -type d -prune -exec rm -rf '{}' +
+find . -name '.idea' -type d -prune -exec rm -rf '{}' +
 
 rm -rf .docker-conf
 rm -rf logs
 
+## Clear python caches
+find . -name '__pycache__' -type d -prune -exec rm -rf '{}' +
+find . -name '.pytest_cache' -type d -prune -exec rm -rf '{}' +
+find . -name '.mypy_cache' -type d -prune -exec rm -rf '{}' +
+
+## Clear java caches
 for Folder in "auth-service" "contas-service" "clientes-service" "gerentes-service"; do
     rm -rf "$Folder"
     rm -rf "services/$Folder/target"
